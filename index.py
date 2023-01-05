9172780a8746fe44412b1f7af9d13442f36b501f
xevckq <xevckq@proton.me>
2023-01-06 00:54:53 +0800

    v1.0.3-debug

--- index.py
+++ index.py
@@ -92,6 +92,9 @@ def webhook(key):
 
     # 从 request 中获取原始数据
     github_signature = request.headers.get("X-Hub-Signature-256")
+    if github_signature is None:
+        return "走开！^-^"
+
     print(github_signature)
     post_data = request.stream.read()
 
